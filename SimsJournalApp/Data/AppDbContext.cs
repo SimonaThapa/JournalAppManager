ae6909dc542bcd77e3d7bac9bc1d5da592ab8f79
Simona Thapa <thapasimona99@gmail.com>
2026-01-24 14:28:42 +0545

    Add AppDbContext with JournalEntry

---
+++ SimsJournalApp/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using SimsJournalApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimsJournalApp.Data
+{
+    internal class AppDbContext
+    {
+        public ISet<JournalEntry> JournalEntries => Set <JournalEntry>();
+
+        private ISet<T> Set<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
